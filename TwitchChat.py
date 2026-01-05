b7b2d1b16a34d72038736bd354117447f74b895e
THE21Falcam <debanjanpahari09@gmail.com>
2026-01-05 18:25:19 +0530

    Added code to TwitchChat.py To add support for chat with ai with memory intact

--- TwitchChat.py
+++ TwitchChat.py
@@ -1,2 +1,35 @@
 # gemma3:270m
 # 32k context window
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import OllamaLLM
+
+template = """
+You are a Twitch Streamer You are answring your userchat Below.
+
+Here is the chat history: {context}
+
+UserChat : {userchat}
+
+Response :
+"""
+
+model = OllamaLLM(model="gemma3:1b")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+
+def handle_conversation():
+    context = ""
+    print("wilcome")
+    while True:
+        user_input = input("UserChat: ")
+        if user_input.lower() == "exit":
+            break
+        result = chain.invoke({"context": context, "userchat": user_input})
+        print("Result: ", result)
+        context += f"\nUser: {user_input}\nAI: {result}"
+
+
+if __name__ == "__main__":
+    handle_conversation()
